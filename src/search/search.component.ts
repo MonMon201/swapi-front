c89a31f32c8c3c68fe337027d7fa8b8d9e38c426
MonMon201 <danyavasilinenko213@gmail.com>
2020-11-02 07:42:39 +0200

    feat: add search component

---
+++ src/search/search.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { names } from '../shared/interfaces/names'
+import { person } from '../shared/interfaces/peson'
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+  form: FormGroup;
+  myControl = new FormControl();
+  options : string[] = [];
+  person: person = {homeworld: 'Homeworld', name : 'Character name', gender : 'n/a',};
+  filteredOptions: Observable<string[]>;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<names[]>(`${environment.API_URL}`)
+    .subscribe(names =>{
+      for(const name of names){
+        this.options[this.options.length] = name.name;
+      }
+    })
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+  }
+
+  submit(){
+    this.http.get<[person]>(`${environment.API_URL}/search-people`, {
+      params: new HttpParams().set('name', this.myControl.value)
+    })
+    .subscribe((person) =>{
+      this.person = person[0];
+    })
+  }
+}
